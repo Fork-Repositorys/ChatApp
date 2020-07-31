b8202897e44faf94b0f8dd780ffcc6f055b65786
Aakash <aakash1350@gmail.com>
2020-07-31 11:23:38 +0530

    Gradle settings updated

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,23 +5,23 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('Hello '),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/pjNxxkwaivMCyMqNB2TU/messages')
+            .snapshots(),
+        builder: (ctx, stremSnapshot) {
+          return ListView.builder(
+            itemCount: 10,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text('Hello'),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/pjNxxkwaivMCyMqNB2TU/messages')
-              .snapshots()
-              .listen((data) {
-            print(data.documents[0]['text']);
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
