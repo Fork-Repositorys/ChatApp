94c0c13d625029122bf0f850db150cf3f61b7552
Aakash <aakash1350@gmail.com>
2020-07-31 20:40:25 +0530

    AuthForm

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -10,17 +10,7 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: Card(
-          margin: EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Form(),
-            ),
-          ),
-        ),
-      ),
+      body: AuthScreen(),
     );
   }
 }
