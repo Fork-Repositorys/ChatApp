403f72cf22077fb1fdb7548f4216602e01af8b1d
Aakash <aakash1350@gmail.com>
2020-07-31 22:58:01 +0530

    authScreen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chatapps/widgets/auth/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -7,12 +8,17 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
   void _submitAuthForm(
     String email,
     String password,
     String username,
     bool isLogin,
-  ) {}
+  ) {
+    if (isLogin) {
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
